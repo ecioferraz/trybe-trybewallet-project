700348306d3e2c65e2ea0f13971e92104e2041a4
Écio Ferraz <ecioferraz@hotmail.com>
2021-12-21 18:26:51 -0300

    Updates

---
+++ precache-manifest.0a413a05177b26d81f4434f3a20d37e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd1fb946c2b6701d2077919d6a78f62",
+    "url": "/trybe-trybewallet-project/index.html"
+  },
+  {
+    "revision": "8dd85c5a5943fbc70369",
+    "url": "/trybe-trybewallet-project/static/css/2.e4d55abd.chunk.css"
+  },
+  {
+    "revision": "814dcf283bd54e81400b",
+    "url": "/trybe-trybewallet-project/static/css/main.7b5394ff.chunk.css"
+  },
+  {
+    "revision": "8dd85c5a5943fbc70369",
+    "url": "/trybe-trybewallet-project/static/js/2.f57281ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-trybewallet-project/static/js/2.f57281ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814dcf283bd54e81400b",
+    "url": "/trybe-trybewallet-project/static/js/main.00f57dce.chunk.js"
+  },
+  {
+    "revision": "b3708845a70ea6e5a467",
+    "url": "/trybe-trybewallet-project/static/js/runtime-main.01005813.js"
+  },
+  {
+    "revision": "dc74ebcc42009a06099326d495eb639f",
+    "url": "/trybe-trybewallet-project/static/media/Business_Startup01.dc74ebcc.jpg"
+  },
+  {
+    "revision": "9f1b1013a3e939fd5277b4a3b632e30b",
+    "url": "/trybe-trybewallet-project/static/media/Businessmen_are_trying_to_push_forward_the_icon_Dollar_money_up.9f1b1013.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/trybe-trybewallet-project/precache-manifest.0a413a05177b26d81f4434f3a20d37e7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trybe-trybewallet-project/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
